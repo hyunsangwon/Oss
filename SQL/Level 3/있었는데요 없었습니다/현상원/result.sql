b338fcef32400008771323aa5b71ac54504149f7
hyunsangwon <hyunsangwon93@gmail.com>
2022-02-01 16:49:28 +0900

    feat : sql 풀어보기~

---
+++ SQL/Level 3/있었는데요 없었습니다/현상원/result.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+-- 코드를 입력하세요
+SELECT
+    ao.ANIMAL_ID,
+    ao.NAME
+FROM ANIMAL_INS AS ai
+INNER JOIN ANIMAL_OUTS AS ao 
+    ON ai.ANIMAL_ID = ao.ANIMAL_ID
+WHERE 
+    ao.DATETIME <= ai.DATETIME
+ORDER BY ai.DATETIME
