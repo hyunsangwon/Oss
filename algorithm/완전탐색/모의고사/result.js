3e140a88acc180c3174973cbd80ae5b7c49a6be0
hyunsangwon <hyunsangwon93@gmail.com>
2022-03-28 17:01:34 +0900

    fix : map으로 변경

--- algorithm/완전탐색/모의고사/result.js
+++ algorithm/완전탐색/모의고사/result.js
@@ -1,27 +1,26 @@
 function solution(answers) {
-    let answer = [];
-    let examCntArr = [0,0,0];  
+    let result = [];
+    let max = 0;
+    let examCntArr = [0,0,0]; 
     let studentList = [
         [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
         [2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
-    for(let i=0; i<answers.length; i++){
-        if(answers[i] === studentList[0][i% studentList[0].length])
+    ];
+    answers.map((e, i) => {
+        if(e === studentList[0][i % studentList[0].length])
             examCntArr[0]++;
-        if(answers[i] === studentList[1][i% studentList[1].length])
+        if(e === studentList[1][i % studentList[1].length])
             examCntArr[1]++;
-        if(answers[i] === studentList[2][i% studentList[2].length])
+        if(e === studentList[2][i % studentList[2].length])
             examCntArr[2]++;
-    }
-    let max = 0
-    examCntArr.forEach(function(e){
+    });
+    examCntArr.forEach((e) => {
         if(e > max) max = e
     });
-    
-    for(let i=0; i<examCntArr.length; i++){
-        if(examCntArr[i] === max) answer.push(i+1)
-    }
-    
-    return answer;
+    examCntArr.map((e,i) => {
+        //1번 부터 시작하므로 i+1
+        if(e === max) result.push(i+1)
+    });
+    return result;
 }
