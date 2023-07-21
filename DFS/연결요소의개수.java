464edf70bc42f93c6da9e8911c3c714d956a4408
hyunsangwon <hyunsangwon93@gmail.com>
2023-07-21 15:04:03 +0900

    feat : DFS 문제 추가

---
+++ DFS/연결요소의개수.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 연결요소의개수 {
+    static int N,M; // N은 노드, M은 연결 선
+    static boolean[][] graph;
+    static boolean[] visited;
+    static int answer;
+
+    public static void dfs(int idx){
+        visited[idx] = true;
+        for(int i=1; i<=N; i++){
+            if(!visited[i] && graph[idx][i]){
+                dfs(idx);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        
+        graph = new boolean[N+1][N+1];
+        visited = new boolean[N+1];
+        
+        //간선 정보 입력
+        int x,y;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=M; i++){
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            graph[x][y] = true;
+            graph[y][x] = true;
+        }
+
+        for(int i=1; i<N; i++){
+            if(!visited[i]){
+                dfs(1);
+                ++answer;
+            }
+        }
+
+        bw.write(String.valueOf(answer - 1)); //출력
+        br.close();
+        bw.close();
+    }
+}
